f45797d580be4ec76c548fbe8c3ba213cdd3728f
c501306014 <c501306014@gmail.com>
2021-03-22 16:16:16 +0900

    フッターとヘッダーを追加

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header() ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>株式会社AJD</title>
-</head>
 
-<body>
 
-</body>
 
-</html>
+<?php get_footer() ?>
