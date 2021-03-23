c9b86278471f669f0229f9ecdf1237d23c043ca0
c501306014 <c501306014@gmail.com>
2021-03-23 20:28:10 +0900

    トップページ　FV実装

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php get_header() ?>
+
+<main>
+    <section class="FV">
+        <div id="visual">
+            <ul class="bxslider">
+                <li style="background-image:url(<?php echo get_template_directory_uri() ?>/images/FV1.png);"></li>
+                <li style="background-image:url(<?php echo get_template_directory_uri() ?>/images/FV2.png);"></li>
+                <li style="background-image:url(<?php echo get_template_directory_uri() ?>/images/FV3.png);"></li>
+                <li style="background-image:url(<?php echo get_template_directory_uri() ?>/images/FV4.png);"></li>
+            </ul>
+        </div>
+        <div id="contents">
+            <img src="<?php echo get_template_directory_uri() ?>/images/catch_fraze.png" alt="キャッチフレーズ">
+        </div>
+    </section>
+
+    <section>
+        <h1>
+            以降開発中
+        </h1>
+    </section>
+</main>
+
+<?php get_footer() ?>
