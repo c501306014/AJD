64688cd49e200b3d784421c5a6f9ab933e6f9968
c501306014 <c501306014@gmail.com>
2021-03-23 20:29:30 +0900

    開発中固定ページの表示を追加

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php get_header() ?>
 
+<main>
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post();
+    ?>
+            <section>
+                <main>
+                    <h1><?php the_title(); ?>（開発中）</h1>
+                    <?php the_content(); ?>
+                </main>
+            </section>
+    <?php endwhile;
+    endif; ?>
+</main>
+
 
 
 
