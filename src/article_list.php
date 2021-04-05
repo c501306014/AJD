c50380f0a71f15b2656adc874612b529fa4f1297
c501306014 <c501306014@gmail.com>
2021-04-05 10:26:19 +0900

    施工事例の投稿一覧を実装

---
+++ src/article_list.php
@@ -0,0 +1,8 @@
+<div class="__item">
+    <a class="post-image" href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail('thumbnail'); ?>
+    </a>
+    <div class="post-title">
+        <h3><?php the_title(); ?></h3>
+    </div>
+</div>
