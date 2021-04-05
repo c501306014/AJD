cdb6d81add4d79bf7c885ec3a03fde7c295a52e0
c501306014 <c501306014@gmail.com>
2021-04-05 10:25:21 +0900

    scroll-to-topボタンを実装

--- js/main.js
+++ js/main.js
@@ -12,4 +12,34 @@ $(function () {
         var windowWidth = $(window).width();
         $('.bxslider li').width(windowWidth)
     });
+
+    scrollToTop();
+
 });
+
+const scrollToTop = function () {
+    let is_show = false;
+
+    $(window).on('load', function () {
+        $('#scroll-to-top').css('display', 'none')
+    })
+
+    $(window).on('scroll  resize', function () {
+        if (200 < $(this).scrollTop()) {
+            if (is_show === false) {
+                $('#scroll-to-top').hide().fadeIn();
+                is_show = true;
+            }
+        } else {
+            if (is_show === true) {
+                $('#scroll-to-top').fadeOut();
+                is_show = false;
+            }
+        }
+    })
+
+    $("#scroll-to-top").click(function () {
+        $("body, html").animate({ scrollTop: 0 }, 300);
+    });
+}
+
