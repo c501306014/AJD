0592308bf3cf6290d286e0e223a0eb13f88da0c2
c501306014 <c501306014@gmail.com>
2021-04-07 11:02:38 +0900

    ハンバーガーメニュー実装

--- js/main.js
+++ js/main.js
@@ -13,16 +13,77 @@ $(function () {
         $('.bxslider li').width(windowWidth)
     });
 
+    init();
+
+    //トップへ戻るボタン
     scrollToTop();
+
+    hamburgerMenu();
 
 });
 
-const scrollToTop = function () {
+const init = function () {
+    $(window).on('load', function () {
+        $('#scroll-to-top').css('display', 'none');
+        $('.hamburger-menu').css('display', 'none');
+    })
+
+    $(window).on("onbeforeunload", function () {
+        alert("unload")
+    })
+}
+
+const hamburgerMenu = function () {
     let is_show = false;
+    const btn = $('.menu-btn');
+    const menu = $('.hamburger-menu');
+    const menuItem = $('.hamburger-menu a');
+
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest(menu).length
+            && $(e.target)[0] != btn[0]
+            && $(e.target)[0] != $('.menu-btn .material-icons')[0]
+            && is_show
+        ) {
+            menu.fadeOut();
+            is_show = false;
+        }
+    });
+
+    btn.on('click', function () {
+        if (!is_show) {
+            menu.hide().fadeIn();
+            is_show = true;
+        } else {
+            menu.fadeOut();
+            is_show = false;
+        }
+    });
+
+    menuItem.on('click', function () {
+        menu.css("display", 'none');
+    });
 
     $(window).on('load', function () {
-        $('#scroll-to-top').css('display', 'none')
+
     })
+
+    // $(window).on('load', function () {
+    //     menu.css('display', 'none');
+    //     is_show = false;
+    // })
+
+    // menuItem.on('click', function () {
+    //     if (is_show) {
+    //         menu.css('display', none);
+    //         is_show = false;
+    //     }
+    // });
+}
+
+const scrollToTop = function () {
+    let is_show = false;
 
     $(window).on('scroll  resize', function () {
         if (200 < $(this).scrollTop()) {
@@ -39,7 +100,7 @@ const scrollToTop = function () {
     })
 
     $("#scroll-to-top").click(function () {
-        $("body, html").animate({ scrollTop: 0 }, 300);
+        $("body, html").animate({ scrollTop: 0 }, 100);
     });
 }
 
